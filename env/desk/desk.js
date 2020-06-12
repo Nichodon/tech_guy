8ee782c21bff36cdb3907cd68e4f1ee646b2caef
Nichodon <brandonsychung@gmail.com>
2020-06-11 20:29:12 -0700

    add desk functionality

--- env/desk/desk.js
+++ env/desk/desk.js
@@ -15,7 +15,7 @@ function loadImages() {
 
 // coordinates are all at the center of the images
 
-const SCALE = 10;
+const SCALE = 5;
 
 let objects = [];
 let canvas, ctx;
@@ -26,31 +26,33 @@ let mouse = {
 }
 let selected;
 
+function updateMouse(e, d) {
+	console.log('aa')
+	let old = { x: mouse.x, y: mouse.y };
+	mouse.x = e.offsetX;
+	mouse.y = e.offsetY;
+	console.log(mouse)
+	if (d && !mouse.down) {
+		for (let i = 0; i < objects.length; i++) {
+			if (isInHitbox(objects[i])) {
+				selected = objects[i];
+				objects.sort((x, y) => x === selected ? -1 : y === selected ? 1 : 0);
+				break;
+			}
+		}
+	} else if (selected && d === undefined) {
+		selected.x += mouse.x - old.x;
+		selected.y += mouse.y - old.y;
+	} else if (!d) {
+		selected = null;
+	}
+	mouse.down = d === undefined ? mouse.down : d;
+}
+
 function setCanvas(c) {
     canvas = c;
     ctx = c.getContext('2d');
     //ctx.imageSmoothingEnabled = false;
-
-    function updateMouse(e, d) {
-        let old = { x: mouse.x, y: mouse.y };
-        mouse.x = e.offsetX //* window.devicePixelRatio;
-        mouse.y = e.offsetY //* window.devicePixelRatio;
-        if (d && !mouse.down) {
-            for (let i = 0; i < objects.length; i++) {
-                if (isInHitbox(objects[i])) {
-                    selected = objects[i];
-                    objects.sort((x, y) => x === selected ? -1 : y === selected ? 1 : 0);
-                    break;
-                }
-            }
-        } else if (selected && d === undefined) {
-            selected.x += mouse.x - old.x;
-            selected.y += mouse.y - old.y;
-        } else if (!d) {
-            selected = null;
-        }
-        mouse.down = d === undefined ? mouse.down : d;
-    }
 
     canvas.addEventListener('mousedown', function(e) { updateMouse(e, true) });
     canvas.addEventListener('mousemove', function(e) { updateMouse(e) });
@@ -68,10 +70,12 @@ function isInHitbox(object) {
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    ctx.fillStyle = '#333';
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-    ctx.fillStyle = canRelease() ? '#444' : '#544';
-    ctx.fillRect(0, canvas.height / 2, canvas.width, canvas.height / 2);
+    ctx.fillStyle = '#111';
+    ctx.fillRect(0, 0, canvas.width / window.devicePixelRatio, canvas.height / window.devicePixelRatio);
+    ctx.fillStyle = canRelease() ? '#222' : '#221';
+    ctx.fillRect(
+		0, canvas.height / 2 / window.devicePixelRatio,
+		canvas.width / window.devicePixelRatio, canvas.height / 2 / window.devicePixelRatio);
 
     for (let i = objects.length - 1; i > -1; i--) {
         drawImage(objects[i], i === 0);
@@ -90,23 +94,21 @@ function drawImage(object, outline) {
         ctx.strokeStyle = '#ccc';
         ctx.lineWidth = 3;
         ctx.strokeRect(
-            object.x - SCALE * object.image.width / 2 - 5 ,//* window.devicePixelRatio,
-            object.y - SCALE * object.image.height / 2 - 5 ,//* window.devicePixelRatio,
-            SCALE * object.image.width + 10 ,//* window.devicePixelRatio,
-            SCALE * object.image.height + 10 );//* window.devicePixelRatio);
+            object.x - SCALE * object.image.width / 2 - 5,
+            object.y - SCALE * object.image.height / 2 - 5,
+            SCALE * object.image.width + 10,
+            SCALE * object.image.height + 10);
     }
 }
 
 function canRelease() {
     for (let i = 0; i < objects.length; i++) {
-        if (objects[i].y > canvas.height / 2) {
+        if (objects[i].y > canvas.height / 2 / window.devicePixelRatio) {
             return false;
         }
     }
     return true;
 }
-
-//window.objects = objects;
 
 export {
 	loadImages,
